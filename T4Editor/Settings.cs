fe1b6c2fd2286d1f99d12abe434a9c580130c51f
Epsilekt <tim.maes91@gmail.com>
2019-05-16 09:17:06 +0200

    Update manifest

---
+++ T4Editor/Settings.cs
@@ -0,0 +1,14 @@
+﻿namespace T4Editor {
+    internal sealed partial class Settings {
+        
+        public Settings() {
+           
+        }
+        
+        private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e) {
+        }
+        
+        private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e) {
+        }
+    }
+}
